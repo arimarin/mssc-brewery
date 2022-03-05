53f8e827e4d9ec1a2c178acdc95bfefa1c012752
Ari Marin <ari.marin@oscar.fi>
2022-03-05 14:52:36 +0200

    Initial commit and first lesson

---
+++ src/main/java/fi/oscar/msscbrewery/services/BeerService.java
@@ -0,0 +1,12 @@
+package fi.oscar.msscbrewery.services;
+
+import fi.oscar.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * Created by Ari on 05.03.2022
+ */
+public interface BeerService {
+	BeerDto getBeerById(UUID beerId);
+}
---
+++ src/main/java/fi/oscar/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,20 @@
+package fi.oscar.msscbrewery.services;
+
+import fi.oscar.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Created by Ari on 05.03.2022
+ */
+@Service
+public class BeerServiceImpl implements BeerService {
+	@Override
+	public BeerDto getBeerById(UUID beerId) {
+		return BeerDto.builder().id(UUID.randomUUID())
+				.beerName("Galaxy Cat")
+				.beerStyle("Pale Ale")
+				.build();
+	}
+}
