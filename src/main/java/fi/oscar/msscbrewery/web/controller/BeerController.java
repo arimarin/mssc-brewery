136ccc264848a2c52793d4f66f5d70b03cae7ffa
Ari Marin <ari.marin@oscar.fi>
2022-03-05 17:05:55 +0200

    Post mapping

--- src/main/java/fi/oscar/msscbrewery/web/controller/BeerController.java
+++ src/main/java/fi/oscar/msscbrewery/web/controller/BeerController.java
@@ -2,10 +2,12 @@ package fi.oscar.msscbrewery.web.controller;
 
 import fi.oscar.msscbrewery.services.BeerService;
 import fi.oscar.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,16 @@ public class BeerController {
 	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
 		return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+	}
+
+	@PostMapping // POST - c reate new beer
+	public ResponseEntity handlePost(BeerDto beerDto) {
+		BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+		HttpHeaders headers = new HttpHeaders();
+		// TODO: add hostname to url
+		headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+		return new ResponseEntity(headers, HttpStatus.CREATED);
 	}
 }
